1bce8da6b9f6410222c7dcf921c44fcc462dc523
Artem <zhiglov.artem@yandex.ru>
2021-07-09 21:28:14 +0500

    rewiew 1

--- scripts/index.js
+++ scripts/index.js
@@ -61,42 +61,15 @@ editPopupSaveButton.addEventListener('click', () => {
   closePopup(editPopup)
 })
 
-function submitFormHandler (evt) {
+function submitFormProfile (evt) {
   evt.preventDefault()
   profileName.textContent = nameInput.value
   profileDescription.textContent = jobInput.value
 }
 
-formPopupEdit.addEventListener('submit', submitFormHandler); 
+formPopupEdit.addEventListener('submit', submitFormProfile); 
 
 // Функциональность для добавления карточек =>
-
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://images.unsplash.com/photo-1535557142533-b5e1cc6e2a5d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1101&q=80'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
 
 const formAddCard = document.querySelector('.form_type_add-card');
 const titleCardInput = document.querySelector('.form__input_type_title-card');
